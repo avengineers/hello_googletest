27e50f1e01ab19ba935369931d08eb3a5c0d2e32
Alexander Mann-Wahrenberg <alexander.mannwahrenberg@gmail.com>
2022-08-16 14:58:07 +0200

    Add simple iut in c

--- hello_test.cc
+++ hello_test.cc
@@ -1,9 +1,17 @@
 #include <gtest/gtest.h>
 
+extern "C" {
+   #include "hello.h"
+}
+
 // Demonstrate some basic assertions.
-TEST(HelloTest, BasicAssertions) {
+TEST(TestSuiteOne, TestOneInTestSuiteOne) {
   // Expect two strings not to be equal.
   EXPECT_STRNE("hello", "world");
   // Expect equality.
   EXPECT_EQ(7 * 6, 42);
+}
+
+TEST(TestSuiteOne, TestTwoInTestSuiteOne) {
+  EXPECT_EQ(1, get_one()) << "Hello";
 }
